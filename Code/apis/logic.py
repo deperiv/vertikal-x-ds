7656d3e0ba5845521643f2d8eb288622eb9cefcc
Daniel <danielpblog2014@gmail.com>
2022-11-29 19:55:03 -0500

    Added api shape to the code

---
+++ Code/apis/logic.py
@@ -0,0 +1,373 @@
+import requests
+import mariadb
+import sys
+import pandas       as pd
+import numpy        as np
+from datetime           import datetime     as dt
+from scipy              import stats
+from pandas.api.types   import is_numeric_dtype
+
+# --- Connect to database ---
+try:
+    conn = mariadb.connect(
+        user="admin_vtx",
+        password="Prueba12#",
+        host="develop-free-vtx.csgmphwpjwca.us-east-1.rds.amazonaws.com",
+        port=3306,
+        database="catalogs"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
+
+# --- Retrieve users and social media usernames ---
+cur.execute("SELECT * FROM users;")
+
+ids = []
+names = []
+instagram_usernames = []
+twitter_usernames = []
+
+for id, name, instagram_username, twitter_username, last_update in cur: 
+    ids.append(id)
+    names.append(name)
+    instagram_usernames.append(instagram_username)
+    twitter_usernames.append(twitter_username)
+    print(f"name: {name}, instagram_username: {instagram_username}, twitter_username: {twitter_username}, last_update: {last_update}")
+
+# --- Extract the data for all the users ---
+
+url_ig = "http://localhost:5000/api/instagram"
+url_tw = "http://localhost:5000/api/twitter"
+
+users_data = []
+
+status = 1
+try:
+    for ix, id in enumerate(ids):
+        user_data_dict = {}
+        user_data_dict["id"] = id
+        user_data_dict["name"] = names[ix]
+        user_data_dict["instagram_username"] = instagram_usernames[ix]
+        user_data_dict["twitter_username"] = twitter_usernames[ix]
+        print(f"Working on {names[ix]}")
+
+        if user_data_dict["instagram_username"] != None:
+            
+            print("Getting IG data")
+            body = {"username": user_data_dict["instagram_username"]}
+            r = requests.post(url_ig, json=body)
+            response = r.json()
+            user_data_dict["instagram_data"] = response
+
+        if user_data_dict["twitter_username"] != None:
+            print("Getting TW data")
+            body = {"username": user_data_dict["twitter_username"]}
+            r = requests.post(url_tw, json=body)
+            response = r.json()
+            user_data_dict["twitter_data"] = response
+        
+        users_data.append(user_data_dict)
+        print(f"Done with {names[ix]}")
+        print("----------------------------------")
+except:
+    status = 0
+    print("---Connection error: Try again in a couple of minutes--")
+
+if status:
+
+    # --- Process the data, compute engagement, percentiles and spi ---
+    def compute_spi_ig(x):
+        spi_ig_weights_dict = {
+            "n_followers": 0.1,
+            "n_posts_total": 0.1, 
+            "n_likes_retrieved": 0.2, 
+            "n_comments_retrieved": 0.1, 
+            "sentiment_instagram": 0.3, 
+            "relative_engagement": 0.2
+        }
+        spi = 0
+        for k in spi_ig_weights_dict.keys():
+            spi += spi_ig_weights_dict[k]*x[k]
+        return spi
+
+    def process_ig_data(data):
+        users_data_instagram = []
+        for user_data in data:
+            if ("instagram_data" in user_data.keys()):
+                dict_data = user_data["instagram_data"].copy()
+                dict_data["user_id"] = user_data["id"]
+                users_data_instagram.append(dict_data)
+            else:
+                empty_dict = {'user_id': users_data["id"],
+                            'username': '',
+                            "user_id_instagram": '',
+                            'n_followers': 0,
+                            'n_following': 0,
+                            'n_posts_total': 0,
+                            'n_posts_retrieved': 0,
+                            'n_likes_total': 0,
+                            'n_likes_retrieved': 0,
+                            'n_comments_total': 0,
+                            'n_comments_retrieved': 0,
+                            'created_at': '',
+                            'sentiment_instagram': 0}
+                users_data_instagram.append(empty_dict)
+            
+        df_ig = pd.DataFrame(users_data_instagram)
+        df_ig["relative_engagement"] = np.clip((df_ig["n_likes_retrieved"] + df_ig["n_comments_retrieved"])/(df_ig["n_followers"])*100, 0, 100)
+        selected_cols = ["n_followers", "n_posts_total", "n_likes_retrieved", "n_comments_retrieved", "sentiment_instagram", "relative_engagement"]
+        df_ig_perc = df_ig.copy()
+        for col in selected_cols:
+            df_ig_perc[col] = df_ig_perc[col].apply(lambda x: stats.percentileofscore(df_ig_perc[col], x))
+        df_ig_perc["spi"] = df_ig_perc[selected_cols].apply(compute_spi_ig, axis=1)
+        spi_ig_columns = ["user_id", "username", "n_followers", "n_posts_total", "n_likes_retrieved", "n_comments_retrieved", "sentiment_instagram", "relative_engagement"]
+        return pd.concat([df_ig[spi_ig_columns], df_ig_perc["spi"], df_ig["created_at"]], axis=1)
+
+    def compute_spi_tw(x):
+        spi_ig_weights_dict = {
+            "n_followers": 0.1,
+            "n_retweets": 0.1, 
+            "n_tweets": 0.1, 
+            "n_retweets_to_user": 0.1, 
+            "n_favorites_to_user": 0.1, 
+            "n_replies_to_user": 0.1,
+            "sentiment_twitter": 0.3,
+            "relative_engagement": 0.1
+        }
+        spi = 0
+        for k in spi_ig_weights_dict.keys():
+            spi += spi_ig_weights_dict[k]*x[k]
+        return spi
+
+    def process_tw_data(data):
+        users_data_twitter = []
+        for user_data in data:
+            if ("twitter_data" in user_data.keys()):
+                dict_data = user_data["twitter_data"].copy()
+                dict_data["user_id"] = user_data["id"]
+                users_data_twitter.append(dict_data)
+            else:
+                empty_dict = {'user_id': user_data["id"],
+                            'username': '',
+                            "n_followers": 0,
+                            'n_retweets': 0,
+                            'n_tweets': 0,
+                            'n_retweets_to_user': 0,
+                            'n_favorites_to_user': 0,
+                            'n_replies_to_user': 0,
+                            'created_at': dt.now(),
+                            'sentiment_twitter': 0}
+                users_data_twitter.append(empty_dict)
+        
+        df_tw = pd.DataFrame(users_data_twitter)
+        df_tw["relative_engagement"] = (df_tw.apply(lambda x: (x["n_favorites_to_user"] + x["n_retweets_to_user"] + x["n_replies_to_user"])/(x["n_followers"])*100 if x["n_tweets"] > 0 else 0, axis=1))
+        selected_cols = ["n_followers", "n_retweets", "n_tweets", "n_retweets_to_user", "n_favorites_to_user", "n_replies_to_user", "sentiment_twitter", "relative_engagement"]
+        df_tw_perc = df_tw.copy()
+        for col in selected_cols:
+            df_tw_perc[col] = df_tw_perc[col].apply(lambda x: stats.percentileofscore(df_tw_perc[col], x))
+        df_tw_perc["spi"] = df_tw_perc[selected_cols].apply(compute_spi_tw, axis=1)
+        spi_tw_columns = ["user_id","username","n_followers","n_retweets","n_tweets","n_retweets_to_user","n_favorites_to_user","n_replies_to_user","sentiment_twitter","relative_engagement"]
+        return pd.concat([df_tw[spi_tw_columns], df_tw_perc["spi"], df_tw["created_at"]], axis=1)
+
+    processed_ig_data = process_ig_data(users_data)
+    processed_tw_data = process_tw_data(users_data)
+
+    # --- Compute spi bonus ---
+
+    def input_ig_data(cur, record):
+        record_dict = record.to_dict('records')[0]
+        cur.execute("INSERT INTO instagram_stats(user_id, instagram_username, n_followers, n_posts_total, n_likes_retrieved, n_comments_retrieved, sentiment, engagement, spi, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, now());",
+        (record_dict["user_id"], record_dict["username"], record_dict["n_followers"], record_dict["n_posts_total"], record_dict["n_likes_retrieved"], record_dict["n_comments_retrieved"], record_dict["sentiment_instagram"], record_dict["relative_engagement"], record_dict["spi"]))
+
+    def retrieve_latest_record_ig(user_id):
+        cur.execute(f"SELECT * FROM instagram_stats WHERE user_id = {user_id} ORDER BY created_at DESC LIMIT 1;") 
+        for record_id, user_id, instagram_username, n_followers, n_posts_total, n_likes_retrieved, n_comments_retrieved, sentiment, engagement, spi, created_at in cur:
+            record_dict = {
+                "user_id": user_id,
+                "username": instagram_username,
+                "n_followers": n_followers,
+                "n_posts_total": n_posts_total,
+                "n_likes_retrieved": n_likes_retrieved,
+                "n_comments_retrieved": n_comments_retrieved,
+                "sentiment_instagram": sentiment,
+                "relative_engagement": engagement,
+                "spi": spi,
+                "created_at": created_at
+            }
+        return pd.DataFrame(record_dict, index=[0])
+        
+
+    def compute_definitive_ig_spi(user_id, new_record, bonus_weight=0.15):
+        cur.execute(f"SELECT COUNT(*) FROM instagram_stats WHERE user_id = {user_id};") 
+        for result in cur: 
+            count_records = result[0]
+        if count_records == 0:
+            return new_record
+        else: 
+            
+            dict_change = {}
+            num_cols = [col for col in new_record.columns if is_numeric_dtype(new_record[col])]
+            dict_change["username"] = new_record["username"].unique()[0]
+
+            latest_record_db = retrieve_latest_record_ig(user_id)
+            
+            for col in num_cols:
+                dict_change[col] = new_record.loc[0][col] - latest_record_db.loc[0][col]
+                
+            df_change = pd.DataFrame(dict_change, index=[0])
+            
+            dict_relative_change = {}
+            dict_change = df_change.to_dict('records')[0]
+            dict_base = latest_record_db.loc[0].to_dict()
+
+            for key in dict_change.keys():
+                if key == "username":
+                    dict_relative_change[key] = dict_change[key]
+                elif key == "created_at" or key == "spi":
+                    pass
+                else:
+                    if dict_base[key] == 0 and dict_change[key] != 0:
+                        dict_relative_change[key] = 1
+                    elif dict_base[key] == 0 and dict_change[key] == 0:
+                        dict_relative_change[key] = 0
+                    else:
+                        dict_relative_change[key] = dict_change[key] / dict_base[key]
+                        
+            dict_ig_spi_bonus_weight = {
+                'n_followers': 0.1,
+                'n_posts_total': 0.1,
+                'n_likes_retrieved': 0.1,
+                'n_comments_retrieved': 0.1,
+                'sentiment_instagram': 0.4,
+                'relative_engagement': 0.2
+            }
+
+            bonus_spi = 0
+            for k in dict_relative_change.keys():
+                if k != "username" and k != "user_id":
+                    bonus_spi += dict_ig_spi_bonus_weight[k]*dict_relative_change[k]
+
+            new_record_upd = new_record.copy()
+            new_record_upd.loc[0, "spi"] = new_record_upd.loc[0, "spi"] + bonus_weight*bonus_spi*100
+
+            return new_record_upd[:1]
+
+    def input_tw_data(cur, record):
+        record_dict = record.to_dict('records')[0]
+        cur.execute("INSERT INTO twitter_stats(user_id, twitter_username, n_followers, n_retweets , n_tweets , n_retweets_to_user , n_favorites_to_user, n_replies_to_user, sentiment, engagement, spi, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, now());",
+        (record_dict["user_id"], record_dict["username"], record_dict["n_followers"], record_dict["n_retweets"], record_dict["n_tweets"], record_dict["n_retweets_to_user"], record_dict["n_favorites_to_user"], record_dict["n_replies_to_user"], record_dict["sentiment_twitter"], record_dict["relative_engagement"], record_dict["spi"]))
+
+    def retrieve_latest_record_tw(user_id):
+        cur.execute(f"SELECT * FROM twitter_stats WHERE user_id = {user_id} ORDER BY created_at DESC LIMIT 1;") 
+        for record_id, user_id, twitter_username, n_followers, n_retweets, n_tweets, n_retweets_to_user, n_favorites_to_user, n_replies_to_user, sentiment, engagement, spi, created_at in cur:
+            record_dict = {
+                "user_id": user_id,
+                "username": twitter_username,
+                "n_followers": n_followers,
+                "n_retweets": n_retweets,
+                "n_tweets": n_tweets,
+                "n_retweets_to_user": n_retweets_to_user,
+                "n_favorites_to_user": n_favorites_to_user,
+                "n_replies_to_user": n_replies_to_user,
+                "sentiment_twitter": sentiment,
+                "relative_engagement": engagement,
+                "spi": spi,
+                "created_at": created_at
+            }
+        return pd.DataFrame(record_dict, index=[0])
+        
+
+    def compute_definitive_tw_spi(user_id, new_record, bonus_weight=0.15):
+        cur.execute(f"SELECT COUNT(*) FROM twitter_stats WHERE user_id = {user_id};") 
+        for result in cur: 
+            count_records = result[0]
+        if count_records == 0:
+            return new_record
+        else: 
+            
+            dict_change = {}
+            num_cols = [col for col in new_record.columns if is_numeric_dtype(new_record[col])]
+            dict_change["username"] = new_record["username"].unique()[0]
+
+            latest_record_db = retrieve_latest_record_tw(user_id)
+            for col in num_cols:
+                dict_change[col] = new_record.loc[0][col] - latest_record_db.loc[0][col]
+                
+            df_change = pd.DataFrame(dict_change, index=[0])
+            
+            dict_relative_change = {}
+            dict_change = df_change.to_dict('records')[0]
+            dict_base = latest_record_db.loc[0].to_dict()
+
+            for key in dict_change.keys():
+                if key == "username":
+                    dict_relative_change[key] = dict_change[key]
+                elif key == "created_at" or key == "spi":
+                    pass
+                else:
+                    if dict_base[key] == 0 and dict_change[key] != 0:
+                        dict_relative_change[key] = 1
+                    elif dict_base[key] == 0 and dict_change[key] == 0:
+                        dict_relative_change[key] = 0
+                    else:
+                        dict_relative_change[key] = dict_change[key] / dict_base[key]
+                        
+            dict_tw_spi_bonus_weight = {
+            'n_followers': 0.1,
+            'n_retweets': 0.1,
+            'n_tweets': 0.1,
+            'n_retweets_to_user': 0.1,
+            'n_favorites_to_user': 0.1,
+            'n_replies_to_user': 0.1,
+            'sentiment_twitter': 0.3,
+            'relative_engagement': 0.1
+        }
+
+            bonus_spi = 0
+            for k in dict_relative_change.keys():
+                if k != "username" and k != "user_id":
+                    bonus_spi += dict_tw_spi_bonus_weight[k]*dict_relative_change[k]
+
+            new_record_upd = new_record.copy()
+            new_record_upd.loc[0, "spi"] = new_record_upd.loc[0, "spi"] + bonus_weight*bonus_spi*100
+
+            return new_record_upd[:1]
+
+
+    # --- Compute general SPI and input the data into the database ---
+
+    df_ig_fully_proc = pd.DataFrame()
+    for user_id in processed_ig_data["user_id"].unique():
+        new_record = processed_ig_data[processed_ig_data["user_id"] == user_id].reset_index().drop("index", axis=1)
+        record_updated = compute_definitive_ig_spi(user_id, new_record)
+        df_ig_fully_proc = pd.concat([df_ig_fully_proc, record_updated], axis=0, ignore_index=True)
+        input_ig_data(cur, record_updated)
+
+    df_tw_fully_proc = pd.DataFrame()
+    for user_id in processed_tw_data["user_id"].unique():
+        new_record = processed_tw_data[processed_tw_data["user_id"] == user_id].reset_index().drop("index", axis=1)
+        record_updated = compute_definitive_tw_spi(user_id, new_record)
+        df_tw_fully_proc = pd.concat([df_tw_fully_proc, record_updated], axis=0, ignore_index=True)
+        input_tw_data(cur, record_updated)
+
+    def input_spi_data(cur, record):
+        record_dict = record.to_dict('records')[0]
+        cur.execute("INSERT INTO spis(user_id, spi, spi_instagram, spi_twitter, created_at) VALUES (?, ?, ?, ?, now());",
+        (record_dict["user_id"], record_dict["spi"], record_dict["spi_instagram"], record_dict["spi_twitter"]))
+
+    spi_df = pd.merge(df_ig_fully_proc[["user_id", "spi"]], df_tw_fully_proc[["user_id", "spi"]], on="user_id", suffixes=["_instagram", "_twitter"])
+    weight_ig = 0.7
+    weight_tw = 0.3
+    spi_df["spi"] = spi_df["spi_instagram"] * weight_ig +  spi_df["spi_twitter"] * weight_tw
+
+    for user_id in spi_df["user_id"].unique():
+        new_record = spi_df[spi_df["user_id"] == user_id].reset_index().drop("index", axis=1)
+        input_spi_data(cur, new_record) 
+
+    conn.commit()
+    conn.close()
---
+++ data_extraction.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+import tweepy
+import requests
+import pandas   as pd
+import numpy    as np
+from fastapi      import FastAPI
+from instagrapi   import Client
+from datetime     import datetime as dt
+
+app = FastAPI(title="Data extraction",
+              description="""Extract data from Instagram and Twitter""",
+              version="0.1.0",)
+
+@app.get("/")
+def home():
+    return {"API":"Data extraction for Instagram and Twitter"}
+
+
+@app.get('/api/status')
+def status():
+    """
+    GET method for API status verification.
+    """
+    
+    message = {
+        "status": 200,
+        "message": [
+            "This API is up and running!"
+        ]
+    }
+    return message
+
+
+
+# INSTAGRAM
+
+cl = Client()
+cl.login("samanthaoakley202212", "1OE0t5@Po9*z")
+
+def extract_text(comment):
+    comments_text_ls = []
+    if len(comment[0]) != 0:
+        comments_text_ls = [comment.text for comment in comment[0]]
+    return comments_text_ls
+
+
+
+# --- TWITTER ---
+
+CONSUMER_KEY = 'P8uR3oU2eTF1hZw2SX1lma8Zw'
+CONSUMER_SECRET = 'Kg85hTJdliuTdFroydQvoRsg7cpr5WZm2MdEQtCz8EjcjG04dp'
+ACCESS_TOKEN = '386339280-5sy1Smvnkw91cBObIwu3ju6aMQJU4B8X8HZNsDSo'
+ACCESS_TOKEN_SECRET = 'WuZNLBRfzZgT6DIDnZOPqaVlt8uMtrvOUJ9aSbUwkmocD'
+
+auth = tweepy.OAuth1UserHandler(
+    CONSUMER_KEY, 
+    CONSUMER_SECRET, 
+    ACCESS_TOKEN, 
+    ACCESS_TOKEN_SECRET
+)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+twitter_username = ""
+
+def get_replies(api, username, tweet_id, max_replies=10, max_attempts=20):
+    replies = tweepy.Cursor(api.search_tweets, q='to:{}'.format(username),
+                                    since_id=tweet_id, tweet_mode='extended').items()
+
+    replies_ls = []
+
+    counter_fetched_rep = 0
+    counter_attempts = 0
+    while counter_fetched_rep < max_replies and counter_attempts < max_attempts:
+        try:
+            reply = replies.next()
+            if not hasattr(reply, 'in_reply_to_status_id_str'):
+                continue
+            if reply.in_reply_to_status_id == tweet_id:
+                replies_ls.append(reply.full_text)
+                counter_fetched_rep = counter_fetched_rep + 1 
+            counter_attempts = counter_attempts + 1 
+
+        except StopIteration:
+            break
+
+        except Exception as e:
+            print("Failed while fetching replies {}".format(e))
+            break
+    return replies_ls
+
+def get_sentiment(comments):
+    url = "http://localhost:5001/api/get_sentiment"
+    body = {"comments": comments}
+
+    r = requests.post(url, json=body)
+    response = r.json()
+    return response
+
+def get_comments_ls(data_dict, mode="ig"):
+    all_comments = []
+    if mode=="ig":
+        for post_id in data_dict['posts_info'].keys():
+            all_comments.extend(data_dict['posts_info'][post_id]["comments_text"])
+    else:
+        for tweet_replies in data_dict["tweets_replies"]:
+            for comment in tweet_replies:
+                all_comments.append(comment)
+    return all_comments
+
+
+@app.post('/api/twitter')
+def get_twitter_data(query: dict):
+
+    username = query["username"]
+    max_tweets = 5
+    max_replies = 10
+    if "max_tweets" in list(query.keys()):
+        max_tweets = query["max_tweets"]
+    if "max_replies" in list(query.keys()):
+        max_replies = query["max_replies"]
+    
+    n_followers = api.search_users(username)[0].followers_count
+    extracted_tweets = []
+
+    for status in tweepy.Cursor(api.search_tweets, 
+                                f"from:{username}",
+                                count=max_tweets).items(max_tweets):
+        extracted_tweets.append(status)
+    retweets = [tweet for tweet in extracted_tweets if "RT @" in tweet.text]
+    tweets = set(extracted_tweets) - set(retweets)
+    n_retweets = len(retweets)
+    n_tweets = len(tweets)
+    n_id_ls = []
+    n_retweets_ls = []
+    n_favorites_ls = []
+    tweet_text_ls = []
+    for tweet in tweets:
+        n_id_ls.append(tweet.id)
+        n_retweets_ls.append(tweet.retweet_count)
+        n_favorites_ls.append(tweet.favorite_count)
+        tweet_text_ls.append(tweet.text)
+    replies_ls = [get_replies(api, username, tweet_id, max_replies=max_replies) for tweet_id in n_id_ls]
+
+    user_data = {
+        "username": username,
+        "n_followers": n_followers,
+        "n_retweets": n_retweets,
+        "n_tweets": n_tweets,
+        "n_retweets_to_user": sum(n_retweets_ls),
+        "n_favorites_to_user": sum(n_favorites_ls),
+        "n_replies_to_user": sum([len(replies_post) for replies_post in replies_ls]),
+        "tweets_text": tweet_text_ls,
+        "tweets_replies": replies_ls,
+        "created_at": dt.now()
+    }
+
+    posts_comments_tw = get_comments_ls(user_data, mode="tw")
+    sentiment_twitter = get_sentiment(posts_comments_tw)["sentiment"]
+    user_data.pop('tweets_text', None)
+    user_data.pop('tweets_replies', None)
+    user_data.pop('posts_info', None)
+    user_data["sentiment_twitter"] = sentiment_twitter
+
+    return user_data
+
+@app.post('/api/instagram')
+def get_ig_data(query: dict): 
+    
+    username = query["username"]
+    max_posts = 10
+    max_comments = 10
+    retrieve_all=False
+
+    if "max_posts" in list(query.keys()):
+        max_posts = query["max_posts"]
+    if "max_comments" in list(query.keys()):
+        max_comments = query["max_comments"]
+    if "retrieve_all" in list(query.keys()):
+        retrieve_all = query["retrieve_all"]
+
+    user_id = cl.user_id_from_username(username)
+    if retrieve_all:
+        posts = cl.user_medias(user_id)
+    else: 
+        posts = cl.user_medias(user_id, max_posts)
+    user_information = cl.user_info(user_id)
+
+    n_followers = user_information.follower_count
+    n_following = user_information.following_count
+    n_posts = user_information.media_count
+
+    # time.sleep(60)
+    posts_info = {}
+    
+    for i, post in enumerate(posts):
+        if i >= max_posts:
+            posts_info[post.id] = {
+                                "n_comments": post.comment_count, 
+                                "n_likes": post.like_count, 
+                                "caption": post.caption_text, 
+                                "comments_text": []
+                                }    
+        else: 
+            posts_info[post.id] = {
+                                    "n_comments": post.comment_count, 
+                                    "n_likes": post.like_count, 
+                                    "caption": post.caption_text, 
+                                    "comments_text": extract_text(cl.media_comments_chunk(post.id, max_amount=max_comments))
+                                    }                              
+    # time.sleep(60)
+    user_data = {
+        "username": username,
+        "user_id_instagram": user_id,
+        "n_followers": n_followers,
+        "n_following": n_following,
+        "n_posts_total": n_posts,
+        "n_posts_retrieved": len(posts_info),
+        "n_likes_total": sum([posts_info[key]["n_likes"] for key in posts_info.keys()]),
+        "n_likes_retrieved": sum([posts_info[key]["n_likes"] for key in list(posts_info.keys())[:max_posts]]),
+        "n_comments_total": sum([posts_info[key]["n_comments"] for key in posts_info.keys()]),
+        "n_comments_retrieved": sum([len(posts_info[key]["comments_text"]) for key in posts_info.keys()]),
+        "created_at": dt.now(),
+        "posts_info": posts_info
+    }
+
+    posts_comments_ig = get_comments_ls(user_data, mode="ig")
+    sentiment_instagram = get_sentiment(posts_comments_ig)["sentiment"]
+    user_data.pop('posts_info', None)
+    user_data["sentiment_instagram"] = sentiment_instagram
+
+    return user_data
